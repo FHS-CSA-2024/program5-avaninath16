43ff447798e9c71b6b5e124fa29d40f96cd3a092
avaninath16 <avani.nath16@gmail.com>
2024-08-20 10:10:36 -0700

    WIP Program5.java

--- Program5.java
+++ Program5.java
@@ -1,6 +1,38 @@
 //import stuff here?
 
 //Your code here
+//import stuff here?
+
+//Your code here
+public class Program5 {
+    public static void main (String [] args){
+        int rMiles = 286;
+        int rgallons = 9;
+        int kMiles = 412;
+        int kgallons = 40;
+        int pMiles = 361; 
+        int pgallons = 40;
+        int bMiles = 161;
+        int bgallons = 11;
+        double rAverage = (rMiles/rgallons)+0.5;
+        double kAverage = (kMiles/kgallons)+0.5;
+        double pAverage = (pMiles/pgallons)+0.5;
+        double bAverage = (bMiles/bgallons)+0.5;
+        
+        system.out.println("Mushroom Cup Prix Racer Average Miles/Per Gallon:");
+        
+        
+    }
+}
+
+
+
+
+//Paste console output below:
+/*
+
+
+*/
 
 //Paste console output below:
 /*
